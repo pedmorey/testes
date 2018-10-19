6cae79f415664758b8b8e50af6b909afb041b659
Pedro Morey Pereira <PPerei18@nextel.com.br>
2018-10-19 18:20:16 -0300

    Enter

--- main.go
+++ main.go
@@ -1,7 +1,22 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+)
 
-func main(){
-	fmt.Println("Hello Go")
+func main() {
+	var x [5]float64
+	x[0] = 98
+	x[1] = 93
+	x[2] = 77
+	x[3] = 82
+	x[4] = 83
+
+	var total float64 = 0
+	for i, value := range x {
+		total += value
+		fmt.Println(i)
+	}
+	fmt.Println(total / float64(len(x)))
+
 }
